07cae02d2f75cb5c38b71b01d93dc49098bc62b8
sreejaajit <sreejaajit@gmail.com>
2017-02-19 08:37:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
 });
 
 app.get('/article-Two', function (req, res) {
